8ad15a62a5f3fd3b51e2aece9491f57719706d41
william lee <will.lee.npc@gmail.com>
2023-08-19 07:39:25 -0400

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution in this file!
+/*beforeEach(() => {
+    employee = {
+      name: "",
+      streetAddress: ""
+      
+    }
+})*/
+const employee = {name:"", streetAddress:""};
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const newEmployee1 = {...employee};
+    newEmployee1[key] = value;
+    
+    //employee.name = key;
+    //employee.streetAddress = value;
+    //console.log(newEmployee1);
+    return newEmployee1;
+
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key){
+    const newEmployee = {...employee};
+    delete newEmployee[key];
+    console.log(newEmployee);
+    return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+}
+//updateEmployeeWithKeyAndValue(employee, 'name', 'test')
